282b9ee88a872fe9fc8a528b8d7c56ea34102a85
mnjese <mjk5949@naver.com>
2025-07-17 18:20:35 +0900

    refactor: 카카오로그인과 로직 통일을 위한 models.py 수정

--- cardgame/models.py
+++ cardgame/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Game(models.Model):
     STATUS_CHOICES = [
@@ -7,12 +8,12 @@ class Game(models.Model):
         ('completed', 'Completed'),
     ]
 
-    attacker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attacker_games')
-    defender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='defended_games')
+    attacker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='attacker_games')
+    defender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='defended_games')
     attacker_card = models.IntegerField()
     defender_card = models.IntegerField(null=True)
-    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='won_games')
-    loser = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='lost_games')
+    winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='won_games')
+    loser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='lost_games')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
 
