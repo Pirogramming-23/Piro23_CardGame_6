d1af261ae1b672ddb3b3158f2ddcfcbac0411947
gicks04 <gichanyoon@gmail.com>
2025-07-18 22:16:12 +0900

    게임정보페이지 수정

--- cardgame/urls.py
+++ cardgame/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('start/', views.game_start, name='game_start'),
     path('<int:pk>/', views.game_detail, name='game_detail'),
     path('<int:pk>/counterattack/', views.game_counterattack, name='game_counterattack'),
-    path('<int:pk>/cancel/', views.cancel_game, name='game_cancel'),
+    path('<int:pk>/cancel/', views.cancel_game, name='cancel_game'),
 ]
