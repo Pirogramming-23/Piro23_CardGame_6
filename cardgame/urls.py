f9ccb2ab2de3f075d22331a35ff39b0e51df429f
galgalrobot <okemerald12@naver.com>
2025-07-18 16:49:43 +0900

    수정 완료

--- cardgame/urls.py
+++ cardgame/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('main/', views.main_page, name='main_page'),
+    path('start/', views.start_game, name='start_game'),
+    path('list/', views.list_game, name='list_game'),
 ]
--- cardgame/views.py
+++ cardgame/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'cardgame/main.html')
+
+def start_game(request):
+    # 게임 시작 로직
+    return render(request, 'cardgame/start.html')
+
+def list_game(request):
+    # 게임 목록 로직
+    return render(request, 'cardgame/list.html')
